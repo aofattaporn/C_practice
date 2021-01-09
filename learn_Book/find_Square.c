7f118eaea5f83838767b10474aeb5781f16b6ab5
aofattapoorn <aofattapon321@gmail.com>
2021-01-10 01:08:54 +0700

    add_in_directory_learnbook

---
+++ learn_Book/find_Square.c
@@ -0,0 +1,8 @@
+#include<stdio.h> 
+int main() { 
+    int x,sx; 
+    printf("Enter a Number :"); 
+    scanf("%d", &x);
+    sx=x*x;
+    printf("Square x= %d\n", sx);
+}
---
+++ learn_Book/find_curcleArea.c
@@ -0,0 +1,10 @@
+#include<stdio.h> 
+int main() { 
+    float area, r; 
+    printf("\n");
+    printf("Enter a Radial :"); 
+    scanf("%f", &r); 
+    area = 3.14 * r * r; 
+    printf("Area a circle = %.2f\n\n", area);
+
+}
