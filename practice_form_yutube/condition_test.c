a5c789c5b3ed45fd5eb32c1a50c7a1a34aed5ae5
aofattapoorn <aofattapon321@gmail.com>
2021-01-11 14:09:19 +0700

    create star

--- practice_form_yutube/condition_test.c
+++ practice_form_yutube/condition_test.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<math.h>
+
 void condition(){ 
     int age; 
     printf("\n");
@@ -48,29 +50,58 @@ int rextangle_star(int w, int h) {
     return area;
 }
 
-void star(int w, int h){ 
+void star_rectangle(int w, int h){ 
+    printf("=============CALL STAR RACTANGLE==================\n");
     for (int item = 0; item < h; item++)
     {
         for (int item2 = 0; item2 < w; item2++)
         {
             printf("*");
-
         }
         printf("\n");
     }  
 }
+void star_pyramid(int result) { 
+    printf("=============CALL STAR PYRAMID==================\n");
+    for(int item = 1; item <= result; item++) { 
+        for(int item2 = 1; item2 <= item; item2 = item2 + 1){ 
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+void star_pyramid_revers(int result) { 
+    printf("=============CALL STAR PYRAMID REVERS==================\n");
+    for(int item = 1; item <= ceil(result/2.0); item++) { 
+        for(int item2 = 1; item2 <= item; item2 = item2 + 1){ 
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int item = 1; item <= (result/2) ; item++){
+        for(int item2 =(result/2); item2 >= item; item2--){ 
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
 
 int main() { 
 
     // rectangle area -> star 
-    printf("=============RECTANGLE FOR STAR==================\n");
+    printf("=============CALL STAR==================\n");
     int w, h;
     printf("Enter widht :");
     scanf("%d", &w);
     printf("Enter hight :");
     scanf("%d", &h);
+
     printf("rectangle area : %d\n", rextangle_star(w, h));
-    star(w, h);
+    star_rectangle(w, h);
+    star_pyramid(rextangle_star(w, h));
+    star_pyramid_revers(rextangle_star(w, h));
     printf("\n");
 
 
