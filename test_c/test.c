f4c56ba29965fcb5850166ee30b263686e14953c
aofattapoorn <aofattapon321@gmail.com>
2021-01-19 23:30:38 +0700

    Test

---
+++ test_c/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+int main(){ 
+    int x = 7; 
+    int y = 3; 
+    float z = 2;
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+
+
+    printf("%d\n", x / y);
+
+
+
+}
